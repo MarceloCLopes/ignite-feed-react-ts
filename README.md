897fd327b11db36157d98cfdfa8ab13a01d9eb81
MarceloCLopes <marc01mcl@gmail.com>
2022-06-28 09:14:39 -0300

    criação, estilizaçõa e finalização do projeto

---
+++ README.md
@@ -0,0 +1,19 @@
+# Ignite Feed
+
+Aplicação de posts em ReactJS usando o framework Vitejs, da Jornada do Ignite Rocketseat, instrutor Diego Fernandes. Tecnologias usadas:
+
+#### tecnologias:
+
+- typescript
+- phosphor-react
+- date-fns
+
+---
+
+## Screenshot
+
+![web](.github/web.png) ![mobile](.github/mobile.png)
+
+---
+
+💜 Ignite Trilha ReactJS 2022
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
+import style from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={style.sidebar}>
+      <img
+        className={style.cover}
+        src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+      />
+
+      <div className={style.profile}>
+        <Avatar src="https://github.com/MarceloCLopes.png" />
+
+        <strong>Marcelo Lopes</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
